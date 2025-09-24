874bce5d4f0c4025c5807c24d55f9998764e53e9
chlhyn1990 <chlhyn1990@gmail.com>
2025-09-24 13:38:41 +0900

    Update NcpApiSignature.java

--- java/NcpApiSignature.java
+++ java/NcpApiSignature.java
@@ -2,40 +2,74 @@ package ncp_auth;
 
 
 import java.util.Base64;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.time.ZoneOffset;
+
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
 public class NcpAuth {
+	private static final ZoneId KOREA_ZONE = ZoneId.of("Asia/Seoul");
+	
 	public static void main(String args[]) {
 		
 		Instant now = Instant.now();
-		LocalDateTime koreaNow = LocalDateTime.ofInstant(now, ZoneId.of("Asia/Seoul"));
-		String timestamp = String.valueOf(koreaNow.toInstant(ZoneOffset.UTC).toEpochMilli());
-		
+		//System.out.println("now:"+now + "\n");
+		String timestamp = String.valueOf(now.atZone(KOREA_ZONE).toInstant().toEpochMilli());
 		String startTime = getStartOfDay(now);
 		String endTime = getEndOfDay(now);
 		
 		String method = "GET";
-		String accessKey = "{accessKey}";			
-		String secretKey = "{secretKey}";
-		String url = "/sms/v2/services/{serviceId}/unsubscribes?startTime="+startTime+"&endTime="+endTime;
+		String accessKey = "";			
+		String secretKey = "";
+		String serviceId = "";
+		String url = "/sms/v2/services/" + serviceId + "/unsubscribes?startTime="+startTime+"&endTime="+endTime;
+		String fullUrl = "https://sens.apigw.ntruss.com" + url;
 		
 		String signature = null;
 		NcpAuth ncpAuth = new NcpAuth();
 		
 		try {
-			
 			signature = ncpAuth.makeSignature(timestamp, method, accessKey, secretKey, url);
+			
+			
+			URL sendUrl = new URL(fullUrl);
+			HttpURLConnection conn = (HttpURLConnection)sendUrl.openConnection();
+			conn.setRequestMethod(method);
+			conn.setDoOutput(true);
+			conn.setRequestProperty("Content-Type", "application/json; charset=utf-8");
+			conn.setRequestProperty("x-ncp-apigw-timestamp", timestamp);
+			conn.setRequestProperty("x-ncp-iam-access-key", accessKey);
+			conn.setRequestProperty("x-ncp-apigw-signature-v2", signature);
+			
+			int responseCode = conn.getResponseCode();
+			
+			if (responseCode == HttpURLConnection.HTTP_OK) {
+                BufferedReader in = new BufferedReader(
+                    new InputStreamReader(conn.getInputStream()));
+                String inputLine;
+                StringBuilder response = new StringBuilder();
+                
+                while ((inputLine = in.readLine()) != null) {
+                    response.append(inputLine);
+                }
+                in.close();
+                System.out.println(response);
+                
+            } else {
+                System.out.println(responseCode + " GET request failed");
+            }
+			
+			conn.disconnect();
+			
 			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
-		System.out.println(signature);
 	}
 	
 	//signature 생성
@@ -66,19 +100,24 @@ public class NcpAuth {
 	}
 	
 	// 하루의 시작 (00:00:00)
-    public static String getStartOfDay(Instant instant) {
-        LocalDateTime startOfDay = instant.atZone(ZoneId.of("Asia/Seoul"))
-                                         .toLocalDate()
-                                         .atStartOfDay();
-        return String.valueOf(startOfDay.toInstant(ZoneOffset.UTC).toEpochMilli());
+    public static String getStartOfDay(Instant now) {
+    	Instant startOfDay = now.atZone(KOREA_ZONE)
+					                .toLocalDate()
+					                .atStartOfDay(KOREA_ZONE)
+					                .toInstant();
+    	//System.out.println("startOfDay:"+startOfDay.atZone(KOREA_ZONE).toLocalDateTime());
+        return String.valueOf(startOfDay.toEpochMilli());
     }
     
     // 하루의 끝 (23:59:59.999999999)
-    public static String getEndOfDay(Instant instant) {
-        LocalDateTime endOfDay = instant.atZone(ZoneId.of("Asia/Seoul"))
-                                       .toLocalDate()
-                                       .atTime(23, 59, 59, 999999999);
-        return String.valueOf(endOfDay.toInstant(ZoneOffset.UTC).toEpochMilli());
+    public static String getEndOfDay(Instant now) {
+    	Instant endOfDay = now.atZone(KOREA_ZONE)
+						                .toLocalDate()
+						                .atTime(23, 59, 59, 999000000)
+						                .atZone(KOREA_ZONE)
+						                .toInstant();
+    	//System.out.println("endOfDay:"+endOfDay.atZone(KOREA_ZONE).toLocalDateTime());
+        return String.valueOf(endOfDay.toEpochMilli());
     }
     
 }
